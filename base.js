d39ff89e60a341035e26174726a3c846fb11aa2f
Auric Vente <mprops@protonmail.com>
2021-08-05 16:18:49 -0600

    nopause n formatmod

--- base.js
+++ base.js
@@ -7,7 +7,6 @@ CW.init = function (a) {
   CW.size = parseInt(style.getPropertyValue("--size"))
   CW.grid_el = document.querySelector("#grid")
   CW.make_grid()
-  CW.key_detection()
   CW.start()
 }
 
@@ -47,134 +46,120 @@ CW.make_grid = function () {
 }
 
 CW.random_int = function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 CW.start = function () {
-    CW.interval = setInterval(function () {
-        CW.iteration()
-    }, CW.speed)
+  CW.interval = setInterval(function () {
+    CW.iteration()
+  }, CW.speed)
 }
 
 CW.iteration = function () {
-    let changed = false
+  let changed = false
 
-    for (let x = 0; x < CW.grid_size; x++) {
-      for (let y = 0; y < CW.grid_size; y++) {
-        let num = 0
+  for (let x = 0; x < CW.grid_size; x++) {
+    for (let y = 0; y < CW.grid_size; y++) {
+      let num = 0
 
-        let y_up = y - 1
-        if (y_up < 0) {
-            y_up = CW.grid_size - 1
+      let y_up = y - 1
+      if (y_up < 0) {
+        y_up = CW.grid_size - 1
+      }
+
+      let y_down = y + 1
+      if (y_down >= CW.grid_size) {
+        y_down = 0
+      }
+
+      let x_left = x - 1
+      if (x_left < 0) {
+        x_left = CW.grid_size - 1
+      }
+
+      let x_right = x + 1
+      if (x_right >= CW.grid_size) {
+        x_right = 0
+      }
+
+      // North
+      if (CW.grid[x][y_up].alive) {
+        num += 1
+      }
+
+      // North West
+      if (CW.grid[x_left][y_up].alive) {
+        num += 1
+      }
+
+      // North East
+      if (CW.grid[x_right][y_up].alive) {
+        num += 1
+      }
+
+      // South
+      if (CW.grid[x][y_down].alive) {
+        num += 1
+      }
+
+      // South West
+      if (CW.grid[x_left][y_down].alive) {
+        num += 1
+      }
+
+      // South East
+      if (CW.grid[x_right][y_down].alive) {
+        num += 1
+      }
+
+      // West
+      if (CW.grid[x_left][y].alive) {
+        num += 1
+      }
+
+      // East
+      if (CW.grid[x_right][y].alive) {
+        num += 1
+      }        
+
+      let item = CW.grid[x][y]
+      item.change_to = "nothing"
+
+      if (item.alive) {
+        if (num < 2) {
+          item.change_to = "dead"
+          changed = true
+        } else if (num > 3) {
+          item.change_to = "dead"
+          changed = true
         }
-
-        let y_down = y + 1
-        if (y_down >= CW.grid_size) {
-            y_down = 0
-        }
-
-        let x_left = x - 1
-        if (x_left < 0) {
-            x_left = CW.grid_size - 1
-        }
-
-        let x_right = x + 1
-        if (x_right >= CW.grid_size) {
-            x_right = 0
-        }
-
-        // North
-        if (CW.grid[x][y_up].alive) {
-            num += 1
-        }
-
-        // North West
-        if (CW.grid[x_left][y_up].alive) {
-            num += 1
-        }
-
-        // North East
-        if (CW.grid[x_right][y_up].alive) {
-            num += 1
-        }
-
-        // South
-        if (CW.grid[x][y_down].alive) {
-            num += 1
-        }
-
-        // South West
-        if (CW.grid[x_left][y_down].alive) {
-            num += 1
-        }
-
-        // South East
-        if (CW.grid[x_right][y_down].alive) {
-            num += 1
-        }
-
-        // West
-        if (CW.grid[x_left][y].alive) {
-            num += 1
-        }
-
-        // East
-        if (CW.grid[x_right][y].alive) {
-            num += 1
-        }        
-
-        let item = CW.grid[x][y]
-        item.change_to = "nothing"
-
-        if (item.alive) {
-            if (num < 2) {
-                item.change_to = "dead"
-                changed = true
-            } else if (num > 3) {
-                item.change_to = "dead"
-                changed = true
-            }
-        } else {
-            if (num === 3) {
-                item.change_to = "alive"
-                changed = true
-            }
+      } else {
+        if (num === 3) {
+          item.change_to = "alive"
+          changed = true
         }
       }
     }
+  }
 
-    if (!changed) {
-        clearInterval(CW.interval)
-        return
-    }
+  if (!changed) {
+    clearInterval(CW.interval)
+    return
+  }
 
-    CW.update()
+  CW.update()
 }
 
 CW.update = function () {
-    for (let row of CW.grid) {
-        for (let item of row) {
-            if (item.change_to === "dead") {
-                item.block.classList.remove("alive")
-                item.alive = false
-            } else if (item.change_to === "alive") {
-                item.block.classList.add("alive")
-                item.alive = true
-            }
-        }
+  for (let row of CW.grid) {
+    for (let item of row) {
+      if (item.change_to === "dead") {
+        item.block.classList.remove("alive")
+        item.alive = false
+      } else if (item.change_to === "alive") {
+        item.block.classList.add("alive")
+        item.alive = true
+      }
     }
-}
-
-CW.key_detection = function () {
-    document.addEventListener('keyup', function (e) {
-        if (e.key === " ") {
-            if (!CW.paused) {
-                clearInterval(CW.interval)
-                CW.paused = true
-            } else {
-                CW.start()
-                CW.paused = false
-            }
-        }
-    })
+  }
 }
