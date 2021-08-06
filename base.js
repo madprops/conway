812b6b68b9141c70311bfc46f69cfb9709c0ca0f
Auric Vente <mprops@protonmail.com>
2021-08-05 19:06:19 -0600

    nonum

--- base.js
+++ base.js
@@ -6,7 +6,6 @@ CW.init = function (a) {
   let style = getComputedStyle(document.body)
   CW.size = parseInt(style.getPropertyValue("--size"))
   CW.grid_el = document.querySelector("#grid")
-  CW.number_el = document.querySelector("#number")
   CW.make_grid()
   CW.start()
 }
@@ -152,8 +151,6 @@ CW.iteration = function () {
 }
 
 CW.update = function () {
-  let number = 0
-
   for (let row of CW.grid) {
     for (let item of row) {
       if (item.change_to === "dead") {
@@ -163,12 +160,6 @@ CW.update = function () {
         item.block.classList.add("alive")
         item.alive = true
       }
-
-      if (item.alive) {
-        number += item.x + item.y
-      }
     }
   }
-
-  CW.number_el.textContent = number
 }
